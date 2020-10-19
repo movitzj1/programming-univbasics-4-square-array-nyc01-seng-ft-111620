785c6faf59fa4129baedeafebff0f43d7f12c4ad
Justin Movitz <movitzj1@montclair.edu>
2020-10-19 17:08:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   array.length.times do |index|
     index = index ** 2
-    new_array << array[index]
+    new_array.push(array[index])
   end
   return new_array
 end
